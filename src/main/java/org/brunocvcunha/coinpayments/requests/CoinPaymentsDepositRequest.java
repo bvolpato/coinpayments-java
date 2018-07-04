ac46963a27df7ec01eefa560d7771bc9991531f4
Zwoerni <mail@zwoerni.net>
2018-07-04 22:44:28 +0200

    added address responses / address commands

---
+++ src/main/java/org/brunocvcunha/coinpayments/requests/CoinPaymentsDepositRequest.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (C) 2018 Bruno Candido Volpato da Cunha (brunocvcunha@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.brunocvcunha.coinpayments.requests;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.brunocvcunha.coinpayments.model.AddressResponse;
+import org.brunocvcunha.coinpayments.model.ResponseWrapper;
+import org.brunocvcunha.coinpayments.requests.base.CoinPaymentsPostRequest;
+
+@AllArgsConstructor
+@Data
+@Builder
+public class CoinPaymentsDepositRequest extends CoinPaymentsPostRequest<ResponseWrapper<AddressResponse>> {
+
+    private String currency;
+
+    @Override
+    public String getUrl () {
+        return "";
+    }
+
+    @Override
+    public String getPayload () {
+        return "cmd=get_deposit_address" + "&currency=" + currency;
+    }
+
+    @Override
+    public ResponseWrapper<AddressResponse> parseResult ( int resultCode, String content ) {
+        ResponseWrapper<AddressResponse> wrapper = parseJson( content, new TypeReference<ResponseWrapper<AddressResponse>>() {} );
+        return wrapper;
+    }
+}
